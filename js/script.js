f3aa94033c0f03273f96e0a9fdaac2b75901bd4d
romiworks <159494755+romiworks@users.noreply.github.com>
2024-02-17 11:45:39 +0800

    client page test updated 20240217

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------
+    File Name: script.js
+---------------------------------------------------------------------*/
+
+function changeImage(x,Image)
+{
+if(x==1)
+{
+image.src='images/fb-icon_hover.png';
+}
+}
+if(x==2)
+{
+image.src='images/fb-icon.png';
+}
